9495f403110aa60cf82b39b52fa7b3540ca2aadc
HFCherish <hf_cherish@163.com>
2018-04-19 17:28:47 +0800

    eureka server

---
+++ eurekaserver/src/main/java/com/tw/shoppify/EurekaServerApp.java
@@ -0,0 +1,17 @@
+package com.tw.shoppify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author hf_cherish
+ * @date 4/19/18
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApp.class, args);
+    }
+}
---
+++ eurekaserver/src/test/java/com/tw/shoppify/EurekaServerAppTest.java
@@ -0,0 +1,35 @@
+package com.tw.shoppify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author hf_cherish
+ * @date 4/19/18
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EurekaServerApp.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class EurekaServerAppTest {
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void should_get_eureka_catalogs() {
+        @SuppressWarnings("rawtypes")
+        ResponseEntity<Map> forEntity = testRestTemplate.getForEntity("/eureka/apps", Map.class);
+        assertThat(forEntity.getStatusCode(), is(HttpStatus.OK));
+
+        System.out.println(forEntity.getBody());
+    }
+}
