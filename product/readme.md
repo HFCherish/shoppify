6320ad85bfbd9f996f5bfbfde5d89f2a5c5c7804
HFCherish <hf_cherish@163.com>
2018-04-19 23:00:10 +0800

    config product as one eureka client

---
+++ product/readme.md
@@ -0,0 +1,4 @@
+坑：
+
+1. eureka 和 spring boot jersey 有依赖冲突，会导致程序起不起来。在 eureka 中配置 exclude 一些依赖
+2. spring `application.yml` 配置中，相同抬头的配置必须配在一块。eg. `spring.application.name` 和 `spring.datasource.url` 都必须放在同一个 `spring` 下
--- product/src/main/java/com/tw/shoppify/product/ProductApp.java
+++ product/src/main/java/com/tw/shoppify/product/ProductApp.java
@@ -1,18 +1,17 @@
 package com.tw.shoppify.product;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author hf_cherish
  * @date 4/17/18
  */
 @SpringBootApplication
-public class ProductApp extends SpringBootServletInitializer {
+@EnableEurekaClient
+public class ProductApp {
     public static void main(String[] args) {
-        new ProductApp()
-                .configure(new SpringApplicationBuilder(ProductApp.class))
-                .run(args);
+        SpringApplication.run(ProductApp.class, args);
     }
 }
